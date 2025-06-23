ba3c0d48599833746c641aaa209a7bff6a150ed2
Hridoy Mnd <hridoymnd10@gmail.com>
2025-06-23 17:32:24 +0600

    basic file folder structure and dependencies install done

---
+++ src/app.ts
@@ -0,0 +1,12 @@
+import Routes from "./modules/routes/routes";
+import express from 'express'
+import cors from 'cors'
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.use(Routes)
+
+
+export default app
---
+++ src/server.ts
@@ -0,0 +1,22 @@
+import app from "./app";
+import config from "./config";
+import mongoose from "mongoose";
+
+//server function
+async function server() {
+  try {
+    await mongoose.connect(config.database_url!);
+
+    console.log(`server connnected with mongodb`);
+
+    //listening example
+    app.listen(config.port, () => {
+      console.log(`Example app listening on port ${config.port}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+//SERVER function call
+server();
