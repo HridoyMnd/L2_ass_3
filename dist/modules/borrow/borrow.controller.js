1151d654d85cd5d1c0b116421744cfd01ddcbee2
Hridoy Mnd <hridoymnd10@gmail.com>
2025-07-06 18:32:48 +0600

    borrow logic recheck and solved

--- dist/modules/borrow/borrow.controller.js
+++ dist/modules/borrow/borrow.controller.js
@@ -8,38 +8,48 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.all_borrow_book = exports.addBorrow = void 0;
 const borrow_model_1 = require("./borrow.model");
+const mongoose_1 = __importDefault(require("mongoose"));
 const book_model_1 = require("../book/book.model");
 // add a book
 const addBorrow = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     try {
         const bookId = req.params.bookId;
-        const borrow_info = req.body;
-        const { quantity } = req.body;
+        const borrow_info = req.body.data;
+        // Destructure and type-convert
+        const quantity = Number(borrow_info.quantity);
+        const book = new mongoose_1.default.Types.ObjectId(borrow_info.book);
+        const dueDate = new Date(borrow_info.dueDate);
+        const b_data = { quantity, book, dueDate };
+        // Book check
         const foundBook = yield book_model_1.Book.findById(bookId);
         if (!foundBook) {
-            res.status(500).json({
+            res.status(404).json({
                 success: false,
-                message: `Book not found with ${bookId}`,
+                message: `Book not found with ID: ${bookId}`,
             });
             return;
         }
+        // Try borrowing
         try {
-            yield (foundBook === null || foundBook === void 0 ? void 0 : foundBook.borrowBooks(quantity));
+            yield foundBook.borrowBooks(quantity);
         }
         catch (error) {
             res.status(400).json({
                 success: false,
-                message: "Book borrow failed",
-                err: `${error}`
+                message: "Book borrow failed (insufficient copies?)",
+                error: error,
             });
             return;
         }
-        const borrowData = yield borrow_model_1.Borrow.create(borrow_info);
-        // response send here
-        res.status(201).send({
+        // Create borrow record
+        const borrowData = yield borrow_model_1.Borrow.create(b_data);
+        res.status(201).json({
             success: true,
             message: "Book borrowed successfully",
             data: borrowData,
@@ -48,8 +58,8 @@ const addBorrow = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     catch (error) {
         res.status(500).json({
             success: false,
-            message: "Book borrowed Failed",
-            error,
+            message: "Book borrowing process failed",
+            error: error,
         });
     }
 });
@@ -69,8 +79,8 @@ const all_borrow_book = (req, res) => __awaiter(void 0, void 0, void 0, function
                     from: "books",
                     localField: "_id",
                     foreignField: "_id",
-                    as: "bookRecord"
-                }
+                    as: "bookRecord",
+                },
             },
             {
                 $unwind: "$bookRecord",
--- src/modules/borrow/borrow.controller.ts
+++ src/modules/borrow/borrow.controller.ts
@@ -1,54 +1,64 @@
-
 import { Request, Response } from "express";
 import { Borrow } from "./borrow.model";
+import mongoose from "mongoose";
 import { Book } from "../book/book.model";
 
 // add a book
 export const addBorrow = async (req: Request, res: Response) => {
   try {
     const bookId = req.params.bookId;
-    const borrow_info = req.body;
-    const { quantity } = req.body;
+    const borrow_info = req.body.data;
+
+    // Destructure and type-convert
+    const quantity = Number(borrow_info.quantity);
+    const book = new mongoose.Types.ObjectId(borrow_info.book as string);
+    const dueDate = new Date(borrow_info.dueDate);
+
+    const b_data = { quantity, book, dueDate };
+
+    // Book check
     const foundBook = await Book.findById(bookId);
 
     if (!foundBook) {
-      res.status(500).json({
+       res.status(404).json({
         success: false,
-        message: `Book not found with ${bookId}`,
+        message: `Book not found with ID: ${bookId}`,
       });
       return;
     }
 
+    // Try borrowing
     try {
-      await foundBook?.borrowBooks(quantity);
+      await foundBook.borrowBooks(quantity);
     } catch (error) {
-      res.status(400).json({
-        success:false,
-        message: "Book borrow failed",
-        err: `${error}`
+       res.status(400).json({
+        success: false,
+        message: "Book borrow failed (insufficient copies?)",
+        error: error,
       });
       return;
     }
-    const borrowData = await Borrow.create(borrow_info);
 
-    // response send here
-    res.status(201).send({
+    // Create borrow record
+    const borrowData = await Borrow.create(b_data);
+
+    res.status(201).json({
       success: true,
       message: "Book borrowed successfully",
       data: borrowData,
     });
+
   } catch (error) {
     res.status(500).json({
       success: false,
-      message: "Book borrowed Failed",
-      error,
+      message: "Book borrowing process failed",
+      error: error,
     });
   }
 };
 
 //get all borrowed book
 export const all_borrow_book = async (req: Request, res: Response) => {
-
   try {
     const summary = await Borrow.aggregate([
       {
@@ -57,14 +67,14 @@ export const all_borrow_book = async (req: Request, res: Response) => {
           totalQuantity: { $sum: "$quantity" },
         },
       },
-  {
-    $lookup: {
-      from: "books",
-      localField: "_id",
-      foreignField: "_id",
-      as: "bookRecord"
-    }
-  },
+      {
+        $lookup: {
+          from: "books",
+          localField: "_id",
+          foreignField: "_id",
+          as: "bookRecord",
+        },
+      },
 
       {
         $unwind: "$bookRecord",
